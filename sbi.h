c5257ab3bf830ec57202f264c5d1d59f8e1ce5ca
josej <josej@central>
2025-09-26 15:13:43 +0000

    name of SBI extensions

--- sbi.h
+++ sbi.h
@@ -1,12 +1,13 @@
 /* SBI Extension IDs */
 #define EID_BASE			0x10
 #define EID_TIME			0x54494D45
-#define EID_IPI				0x735049
-#define EID_RFENCE			0x52464E43
-#define EID_HSM				0x48534D
-#define EID_DBCN			0x4442434E  // Debug Console Extension
+#define EID_IPI				0x735049	// Interprocessor Interrupt	
+#define EID_RFNC			0x52464E43	// Remote fence operations
+#define EID_HSM				0x48534D	// Hart State Management
+#define EID_DBCN			0x4442434E	// Debug Console Extension
 
 /* SBI function ID for DBCN extension */
-#define FID_DBCN_WRITE	0
+#define FID_DBCN_WRITE			0
 
-
+/* SBI function ID for TIME extension */
+#define FID_TIME_SET_TIMER 		0
